2a8893cc6675098fd3399fffe384e616e53c5c48
SyamayDev <xrpltritech@gmail.com>
2025-09-03 08:28:21 +0700

    First commit untuk project versi baru

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Crud_Perusahaan/';
+$config['base_url'] = 'http://localhost/Perpustakaan/';
 
 /*
 |--------------------------------------------------------------------------
