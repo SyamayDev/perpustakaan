68b1bbe23a802b2fec995c6ba6b6859c7f0a227b
SyamayDev <xrpltritech@gmail.com>
2025-09-03 09:19:30 +0700

    Refactor login controller to utilize m_data model for authentication and remove unused Crud controller

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,45 +3,61 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login extends CI_Controller
 {
-    function __construct()
-    {
+    function __construct(){
         parent::__construct();
-        $this->load->model('m_login');
-        $this->load->helper('url');
-        $this->load->library('session');
     }
 
-    public function index()
-    {
+    public function index() {
         $this->load->view('v_login');
     }
 
-    public function aksi_login()
-    {
+    public function aksi_login(){
         $username = $this->input->post('username');
         $password = $this->input->post('password');
-        $where = array(
-            'username' => $username,
-            'password' => md5($password)
-        );
+        $sebagai = $this->input->post('sebagai');
 
-        $cek = $this->m_login->cek_login("admin", $where)->num_rows();
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
 
-        if ($cek > 0) {
-            $data_session = array(
+        if ($this->form_validation->run() != false) {
+            $where = array(
                 'username' => $username,
-                'status' => "login"
+                'password' => md5($password)
             );
-            $this->session->set_userdata($data_session);
-            redirect(base_url('admin'));
-        } else {
-            echo "Username dan password salah !";
-        }
-    }
 
-    public function logout()
-    {
-        $this->session->sess_destroy();
-        redirect(base_url('login'));
+            if ($sebagai == 'admin') {
+                $cek = $this->m_data->cek_login("admin",$where)->num_rows();
+                $data = $this->m_data->cek_login("admin",$where)->row();
+
+                if ($cek >  0) {
+                    $data_session = array(
+                        'id' => $data->id,
+                        'username' => $data->username,
+                        'status' => "Admin Login"
+                    );
+
+                    $this->session->set_userdata($data_session);
+                    redirect(base_url("admin"));
+                } else {
+                    redirect(base_url(). 'login?alert=gagal');
+                }
+            } else if ($sebagai == 'petugas') {
+                $cek = $this->m_data->cek_login("petugas",$where)->num_rows();
+                $data = $this->m_data->cek_login("petugas",$where)->row();
+
+                if ($cek >  0) {
+                    $data_session = array(
+                        'id' => $data->id,
+                        'username' => $data->username,
+                        'status' => "Petugas Login"
+                    );
+
+                    $this->session->set_userdata($data_session);
+                    redirect(base_url("petugas"));
+                } else {
+                    redirect(base_url(). 'login?alert=gagal');
+                }
+            }
+        }
     }
 }
