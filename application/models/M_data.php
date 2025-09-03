cfebedf5fc9fe000fa298fac0bdb77b37e781889
SyamayDev <xrpltritech@gmail.com>
2025-09-03 08:34:14 +0700

    new m_data

--- application/models/M_data.php
+++ application/models/M_data.php
@@ -1,25 +1,17 @@
 <?php
 class M_data extends CI_Model{
 
-    function tampil_data(){
-        return $this->db->get('karyawan');
+    // FUNGSI CRUD
+
+    // fungsi untuk mengambil data dari database
+    public function get_data($table){
+        return $this->db->get($table);
     }
 
-    function input_data($data,$table){
+    // fungsi untuk menginput data ke database
+    public function insert_data($data,$table){
         $this->db->insert($table,$data);
     }
 
-    function hapus_data($where,$table){
-        $this->db->where($where);
-        $this->db->delete($table);
-    }
-
-    function edit_data($where,$table){
-        return $this->db->get_where($table,$where);
-    }
-
-    function update_data($where,$data,$table){
-        $this->db->where($where);
-        $this->db->update($table,$data);
-    }
+    // fungsi untuk edit data 
 }
