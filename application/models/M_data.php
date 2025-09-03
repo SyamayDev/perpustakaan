7e336d1681a9bab89ca8f6583c01f72a33cd2e27
SyamayDev <xrpltritech@gmail.com>
2025-09-03 08:43:40 +0700

    add cek login

--- application/models/M_data.php
+++ application/models/M_data.php
@@ -13,5 +13,24 @@ class M_data extends CI_Model{
         $this->db->insert($table,$data);
     }
 
-    // fungsi untuk edit data 
+    // fungsi untuk mengedit data
+    public function edit_data($where,$table){
+        return $this->db->get_where($table,$where);
+    }
+
+    // fungsi untuk mengupdate atau mengubah data di database
+    public function update_data($where,$data,$table){
+        $this->db->where($where);
+        $this->db->update($table,$data);
+    }
+
+    // fungsi untuk menghapus data dari database
+    public function delete_data($where,$table){
+        $this->db->where($table,$where);
+    }
+    // AKHIR FUNGSI CRUD
+
+    function cek_login($table,$where){
+        return $this->db->get_where($table,$where);
+    }
 }
